c8a79206b28f2d1ed02fa7aeda2057988c47d512
STANDARDYUNLAPT\User <kijoonyun@smartfactory.co.kr>
2025-07-06 14:39:22 +0900

    Init: application title & icon configuration

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <h1>Pikachu Run!</h1>
+      <h1>Run Pikachu!</h1>
       <div className='game-container'>{/* 게임 요소들이 들어갈 곳 */}</div>
     </div>
   );
