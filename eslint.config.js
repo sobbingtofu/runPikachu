5ea75ef4ffe0ab5a49f872b6140a02d10a8c6cf3
sobbingtofu <kukubasak@naver.com>
2025-07-06 11:53:26 +0900

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    ignores: ['dist', '.next', 'node_modules'], // ✨ 추가: 무시할 파일/폴더를 명시합니다.
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      eslintPluginPrettierRecommended,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+]);
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Pikachu Run!</h1>
+      <div className='game-container'>{/* 게임 요소들이 들어갈 곳 */}</div>
+    </div>
+  );
+}
+
+export default App;
