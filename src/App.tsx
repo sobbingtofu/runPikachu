5c93382a1ac9403a5a0c157c3539ca824d1878b8
sobbingtofu <kukubasak@naver.com>
2025-07-26 17:27:41 +0900

    Feat: Separate collision detection logic to custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { useGameStore } from './store/gameStore';
 import useGameFundamentals from './hooks/useGameFundamentals';
 import usePikachuJump from './hooks/usePikachuJump';
 import useGenerateObastacles from './hooks/useGenerateObastacles';
+import useProcessCollision from './hooks/useProcessCollision';
 
 function App() {
   const { GAME_AREA_WIDTH, gameFundamentals, pikachuState } = useGameStore();
@@ -12,6 +13,8 @@ function App() {
   useGameFundamentals(); // 키 이벤트 등 부가 로직 실행
   usePikachuJump(); // 점프 애니메이션 실행
   useGenerateObastacles(); // 장애물 생성 및 이동 로직 실행
+
+  const { isCollision, pikachuHitbox } = useProcessCollision();
 
   const gameAreaStyle = {
     width: `${GAME_AREA_WIDTH}px`,
@@ -22,35 +25,6 @@ function App() {
     border: '2px solid black',
   };
 
-  const pikachuHitbox = {
-    x: pikachuState.pikachuValueX + 20, // 피카츄의 위치 + 여유 공간
-    y: pikachuState.pikachuValueY,
-    width: pikachuState.pikachuWidth ? pikachuState.pikachuWidth - 30 : 80,
-    height: pikachuState.pikachuHeight || 53,
-  };
-
-  function isColliding(
-    pikachuHitbox: { x: number; y: number; width: number; height: number },
-    obstacle: { x: number; y: number; width: number; height: number },
-  ) {
-    const collision =
-      pikachuHitbox.x < obstacle.x + obstacle.width &&
-      pikachuHitbox.x + pikachuHitbox.width > obstacle.x &&
-      pikachuHitbox.y < obstacle.y + obstacle.height &&
-      pikachuHitbox.y + pikachuHitbox.height > obstacle.y;
-
-    return collision;
-  }
-
-  // 장애물 충돌 여부 계산
-  const isCollision = gameFundamentals.obstacles.some((obs) =>
-    isColliding(pikachuHitbox, {
-      x: obs.positionX,
-      y: obs.positionY,
-      width: obs.width,
-      height: obs.height,
-    }),
-  );
   return (
     <div className='App'>
       <h1>Run Pikachu!</h1>
---
+++ src/hooks/useProcessCollision.ts
@@ -0,0 +1,38 @@
+import { useGameStore } from '../store/gameStore';
+
+const useProcessCollision = () => {
+  const { gameFundamentals, pikachuState } = useGameStore();
+
+  const pikachuHitbox = {
+    x: pikachuState.pikachuValueX + 20, // 피카츄의 위치 + 여유 공간
+    y: pikachuState.pikachuValueY,
+    width: pikachuState.pikachuWidth ? pikachuState.pikachuWidth - 30 : 80,
+    height: pikachuState.pikachuHeight || 53,
+  };
+
+  function isColliding(
+    pikachuHitbox: { x: number; y: number; width: number; height: number },
+    obstacle: { x: number; y: number; width: number; height: number },
+  ) {
+    const collision =
+      pikachuHitbox.x < obstacle.x + obstacle.width &&
+      pikachuHitbox.x + pikachuHitbox.width > obstacle.x &&
+      pikachuHitbox.y < obstacle.y + obstacle.height &&
+      pikachuHitbox.y + pikachuHitbox.height > obstacle.y;
+
+    return collision;
+  }
+
+  // 장애물 충돌 여부 계산
+  const isCollision = gameFundamentals.obstacles.some((obs) =>
+    isColliding(pikachuHitbox, {
+      x: obs.positionX,
+      y: obs.positionY,
+      width: obs.width,
+      height: obs.height,
+    }),
+  );
+  return { isCollision, pikachuHitbox };
+};
+
+export default useProcessCollision;
